c5b3f1ed1c2b77822b82253c432553e8ab3f00a6
ayushman3704 <ayushman3704@gmail.com>
2025-11-14 19:17:45 +0530

    Delete api directory

--- api/[...slug].js
+++
@@ -1,9 +0,0 @@
-import serverless from "serverless-http";
-import app from "../backend/src/server.js";
-
-const handler = serverless(app);
-
-export default async function (req, res) {
-  // serverless-http returns a handler compatible with (req, res)
-  return handler(req, res);
-}
